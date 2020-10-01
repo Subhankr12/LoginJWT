8176b3c1b228755e9d8cc7e2a2c37bbcb6e2b7df
Jagdish <jatin6972@gmail.com>
2020-10-01 21:09:22 +0530

    changed user schema

--- models/User.js
+++ models/User.js
@@ -1,28 +1,31 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
     min: 6,
-    max: 255
+    max: 255,
+    trim: true,
   },
   email: {
     type: String,
     required: true,
     min: 6,
-    max: 255
+    max: 255,
+    trim: true,
   },
   password: {
     type: String,
     required: true,
     max: 1024,
-    min: 8
+    min: 8,
+    trim: true,
   },
   date: {
     type: Date,
-    default: Date.now
-  }
-})
+    default: Date.now,
+  },
+});
 
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model("User", userSchema);
